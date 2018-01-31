a877627332d86d0383bbb10ae954b0264ed7c6b7
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-01-31 11:27:36 +0900

    add slim

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def index
-  	@comments = Comment.where(post_id: params[:post_id]).order(created_at: 'DESC')
+  	@comments = Comment.where(post_id: params[:post_id]).order(created_at: 'ASC')
   	@comment = Comment.new
   	render(layout: "posts_head")
   	respond_to do |format|
@@ -12,13 +12,14 @@ class CommentsController < ApplicationController
 
   def create
   	@comment = Comment.new(contents: params[:contents], user_id: @current_user.id, post_id: params[:post_id])
+  	@comments = Comment.where(post_id: params[:post_id]).order(created_at: 'ASC')
   	if @comment.save
   		respond_to do |format|
   			format.html
   			format.js
   		end
   	else
-  		render("comments/index")
+  		render(partial: "comments/index")
   	end
   end
  
--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,31 +1,32 @@
 class GoodsController < ApplicationController
-	before_action :pushed_good_user
+  before_action :pushed_good_user
 
   def create
   	@good = Good.new(user_id: @current_user.id, post_id: params[:post_id])
-  	if @good_user
-   		flash[:notice] = "2回評価することは出来ません。"
-   		redirect_to("/posts/#{params[:post_id]}")
-  	elsif @good.save
-  		flash[:notice] = "成功しました。"
-      redirect_to("/posts/#{params[:post_id]}")
-  	else
-  		flash[:notice] = "失敗しました。"
-  	 	redirect_to("/posts/#{params[:post_id]}")
-  	end
-  end
-
-  def destroy
-    if @good_user.destroy
-      flash[:notice] = "成功"
-      redirect_to("/posts/#{params[:post_id]}")
+    respond_to do |format|
+  	 if @good_user
+      format.js
+     else
+      @good.save
+      format.js
+  	 end
     end
   end
 
-  private
+  def destroy
+    respond_to do |format|
+      if @good_user
+        @good_user.destroy
+        format.js
+      else
+        format.js
+      end
+    end
+  end
 
   def pushed_good_user
     @good_user = Good.find_by(user_id: @current_user.id, post_id: params[:post_id])
+    @post = Post.find_by(id: params[:post_id])
   end
 
 end
