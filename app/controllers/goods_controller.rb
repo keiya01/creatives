8f57a037305cd1e544221bc16d06654fe9feeeb8
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-02-04 08:39:05 +0900

    add point action

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -3,12 +3,30 @@ class GoodsController < ApplicationController
 
   def create
   	@good = Good.new(user_id: @current_user.id, post_id: params[:post_id])
+    @post = Post.find_by(id: params[:post_id])
+    @user = User.find_by(id: @post.user_id)
+    @remove_point = Point.find_by(user_id: @current_user.id)
+    @add_point = Point.find_by(user_id: @user.id)
     respond_to do |format|
   	 if @good_user
       format.js
-     else
+     elsif @remove_point.total == 0
+      flash[:notice] = "ポイントが足りません。"
+      redirect_to "/posts/index"
+     elsif @remove_point.total >= 1
       @good.save
-      format.js
+      @remove_point.total -= 1
+      if @remove_point.save
+        @add_point.total += 1
+        @add_point.save
+        format.js
+      else
+        flash[:notice] = "エラーが発生しました。"
+        redirect_to "/posts/index"
+      end
+     else
+      flash[:notice] = "エラーが発生しました。"
+      redirect_to "/posts/index"
   	 end
     end
   end
