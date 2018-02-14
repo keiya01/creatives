74fe40a665796952c916270c8c0d7118d129e3d1
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-02-14 16:45:18 +0900

    add agreement.html.slim

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
-  before_action :brock_current_user
+  before_action :brock_current_user, {only:[:top]}
   layout 'home'
   
   def top
+  end
+
+  def agreement
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get 'users/:id' => 'users#show'
 
   get '/' => 'home#top'
+  get 'agreement' => 'home#agreement'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
