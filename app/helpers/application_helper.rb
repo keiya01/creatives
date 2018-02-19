02bb3ea57f50ed1a68e97969c2c73acd12dc2e42
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-02-19 12:36:04 +0900

    add instantiate URL method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
 	def simple_time(time)
 		time.strftime("%Y-%m-%d　%H:%M　")
 	end 
+
+	require "uri"
+
+	def text_url_to_link(text)
+  		URI.extract(text, ['http','https']).uniq.each do |url|
+    		sub_text = link_to url, url, target: '_blank'
+
+		    text.gsub!(url, sub_text)
+	  	end
+
+  		return text
+	end
 end
